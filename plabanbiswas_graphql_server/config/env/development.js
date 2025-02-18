661fec999ae6930f2eb265b7f0c0ebbe50df0b5e
Plaban Biswas <plabanbiswas96@outlook.com>
2025-02-18 16:35:14 -0500

    - ADDED: Server Base Setup

---
+++ plabanbiswas_graphql_server/config/env/development.js
@@ -0,0 +1,5 @@
+// Set the 'development' environment configuration object
+module.exports = {
+	db: 'mongodb://localhost:27017/plabanbiswas-db-dev',
+	sessionSecret: 'developmentSessionSecret'
+};
---
+++ plabanbiswas_graphql_server/config/env/test.js
@@ -0,0 +1,5 @@
+// Set the 'test' environment configuration object
+module.exports = {
+	db: 'mongodb://localhost:27017/plabanbiswas-db-test',
+	sessionSecret: 'testSessionSecret'
+};
